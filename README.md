93612c72fa89f2776e5b09639925cfef3ae31238
fahmi <fahmi@localhost.localdomain>
2023-09-23 12:31:21 +0700

    temp file operation

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-#ImreFS
+# ImreFS
 
-Simple filesystem using Rust
+Simple filesystem using Rust, challenge come from here:
+[ImreFS Challenge](https://likeable-launch-ac7.notion.site/ImreFS-Part-1-Interprocess-Communication-b98c36f78771435d8b587b339630ad70)
 
 - [x] CLI arguments
+- [x] Create a temp file, read, and delete the file
 - [ ] Init process
 - [ ] Send message
 - [ ] Stop process
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,46 @@
+use std::{
+    fs::{remove_file, File, OpenOptions},
+    io::Write,
+    process::exit,
+};
+
 use imrefs::args::Args;
 
 fn main() {
     let args = Args::parse();
+    let tempfile_name = format!("tmp/imrefs-{}.tmp", args.file_name);
 
-    let arg_type: String = match args.arg_type {
-        imrefs::args::ArgType::Init => "Init".to_string(),
-        imrefs::args::ArgType::Send => "Send".to_string(),
-        imrefs::args::ArgType::Stop => "Stop".to_string(),
-        imrefs::args::ArgType::Others => "Others".to_string(),
+    match args.arg_type {
+        imrefs::args::ArgType::Init => {
+            if let Err(e) = File::create(tempfile_name) {
+                eprintln!("error create temp file: {}", e);
+                exit(1);
+            }
+        }
+        imrefs::args::ArgType::Send => {
+            let mut file = match OpenOptions::new()
+                .create(true)
+                .write(true)
+                .truncate(true)
+                .open(tempfile_name)
+            {
+                Ok(f) => f,
+                Err(e) => {
+                    eprintln!("error open temp file: {}", e);
+                    exit(1)
+                }
+            };
+
+            if let Err(e) = file.write_all(args.message.as_bytes()) {
+                eprintln!("cannot write to temp file: {}", e);
+            }
+        }
+        imrefs::args::ArgType::Stop => {
+            if let Err(e) = remove_file(tempfile_name) {
+                eprintln!("error create temp file: {}", e);
+                exit(1);
+            }
+        }
+        imrefs::args::ArgType::Others => {}
     };
-
-    eprint!(
-        "filesystem name: {}\nmessage: {}\ntype: {}\n",
-        args.file_name, args.message, arg_type
-    )
 }
