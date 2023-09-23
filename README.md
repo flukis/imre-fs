74afbc6a7fb199c082516e87ab3fbf7a2329e76c
fahmi <fahmi@localhost.localdomain>
2023-09-23 21:03:13 +0700

    init and stop process

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Simple filesystem using Rust, challenge come from here:
 
 - [x] CLI arguments
 - [x] Create a temp file, read, and delete the file
-- [ ] Init process
-- [ ] Send message
-- [ ] Stop process
+- [x] Init process
+- [x] Send message
+- [x] Stop process
+- [ ] receive SIGKILL from command `kill THE_PID`
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,163 @@
+use std::fs::OpenOptions;
+use std::io::Read;
+use std::os::unix::net::{UnixListener, UnixStream};
 use std::{
-    fs::{remove_file, File, OpenOptions},
+    fs::{remove_file, File},
     io::Write,
     process::exit,
+    thread::spawn,
 };
 
 use imrefs::args::Args;
+use libc::getpid;
+use signal_hook::iterator::Signals;
 
 fn main() {
+    let mut signals = Signals::new(&[libc::SIGTERM]).expect("Failed to initialize signal handler");
+
     let args = Args::parse();
-    let tempfile_name = format!("tmp/imrefs-{}.tmp", args.file_name);
+    let socket_name = format!("/tmp/imrefs-{}.sock", args.file_name);
+    let tempfile_name = format!("/tmp/imrefs-{}.tmp", args.file_name);
+    let socket_name_close = format!("/tmp/imrefs-{}.sock", args.file_name);
+    let tempfile_name_sloce = format!("/tmp/imrefs-{}.tmp", args.file_name);
+    spawn(move || {
+        for signal in signals.forever() {
+            match signal {
+                libc::SIGTERM => {
+                    println!("Received SIGTERM signal.");
+                    cleanup(&socket_name_close, &tempfile_name_sloce)
+                }
+                _ => {}
+            }
+        }
+    });
 
     match args.arg_type {
         imrefs::args::ArgType::Init => {
-            if let Err(e) = File::create(tempfile_name) {
-                eprintln!("error create temp file: {}", e);
+            if let Err(e) = File::create(&tempfile_name) {
+                eprintln!("Error can't create file: {}", e);
                 exit(1);
+            };
+            match unsafe { libc::fork() } {
+                -1 => {
+                    eprintln!("fork failed");
+                    exit(1);
+                }
+                0 => {
+                    if unsafe { libc::setsid() } == -1 {
+                        eprintln!("Failed to create new session");
+                        exit(1);
+                    }
+                    let listener = match UnixListener::bind(&socket_name) {
+                        Ok(listener) => listener,
+                        Err(err) => {
+                            eprintln!("Failed to bind to Unix socket: {}", err);
+                            exit(1);
+                        }
+                    };
+                    let child_pid = unsafe { getpid() };
+                    println!(
+                        "Filesystem {} successfully created with PID: {}",
+                        socket_name, child_pid
+                    );
+                    for stream in listener.incoming() {
+                        match stream {
+                            Ok(mut stream) => {
+                                let mut response = String::new();
+                                if let Err(err) = stream.read_to_string(&mut response) {
+                                    eprintln!("Failed to read from socket: {}", err);
+                                    exit(1);
+                                }
+                                if response == "cmd:stop" {
+                                    // remove the file socket
+                                    if let Err(err) = remove_file(&socket_name) {
+                                        eprintln!("Failed to remove file socket: {}", err);
+                                        exit(1);
+                                    }
+                                    // remove the file socket
+                                    if let Err(err) = remove_file(&tempfile_name) {
+                                        eprintln!("Failed to remove temporary file: {}", err);
+                                        exit(1);
+                                    }
+
+                                    println!("Filesystem {} succesfully removed", &socket_name);
+                                    exit(0);
+                                }
+                                // response = response.replace("msg:", "");
+
+                                let mut file = match OpenOptions::new()
+                                    .create(true)
+                                    .write(true)
+                                    .truncate(true)
+                                    .open(&tempfile_name)
+                                {
+                                    Ok(f) => f,
+                                    Err(e) => {
+                                        println!("Error can't open file: {}", e);
+                                        exit(1);
+                                    }
+                                };
+
+                                let response = response.replace("msg:", "");
+                                println!("Data successfully written to file: {}", &tempfile_name);
+
+                                if let Err(e) = file.write_all(response.as_bytes()) {
+                                    println!("Error can't write to file: {}", e);
+                                    continue;
+                                };
+                            }
+                            Err(err) => {
+                                eprintln!("Failed to accept connection: {}", err);
+                            }
+                        }
+                    }
+                }
+                _ => {
+                    exit(0);
+                }
             }
         }
         imrefs::args::ArgType::Send => {
-            let mut file = match OpenOptions::new()
-                .create(true)
-                .write(true)
-                .truncate(true)
-                .open(tempfile_name)
-            {
-                Ok(f) => f,
-                Err(e) => {
-                    eprintln!("error open temp file: {}", e);
-                    exit(1)
+            let mut stream = match UnixStream::connect(socket_name) {
+                Ok(s) => s,
+                Err(err) => {
+                    eprintln!("Failed connect to Unix socket: {}", err);
+                    exit(1);
                 }
             };
-
-            if let Err(e) = file.write_all(args.message.as_bytes()) {
-                eprintln!("cannot write to temp file: {}", e);
-            }
+            let message_formatted = format!("msg:{}", args.message);
+            if let Err(e) = stream.write_all(message_formatted.as_bytes()) {
+                println!("Error can't write to socket: {}", e);
+                exit(1);
+            };
         }
         imrefs::args::ArgType::Stop => {
-            if let Err(e) = remove_file(tempfile_name) {
-                eprintln!("error create temp file: {}", e);
+            let mut stream = match UnixStream::connect(socket_name) {
+                Ok(s) => s,
+                Err(err) => {
+                    eprintln!("Failed connect to Unix socket: {}", err);
+                    exit(1);
+                }
+            };
+            if let Err(e) = stream.write_all("cmd:stop".as_bytes()) {
+                println!("Error can't write to socket: {}", e);
                 exit(1);
-            }
+            };
         }
         imrefs::args::ArgType::Others => {}
     };
+}
+
+fn cleanup(socket_name: &str, tempfile_name: &str) {
+    // remove the file socket
+    if let Err(err) = remove_file(socket_name) {
+        eprintln!("Failed to remove file socket: {}", err);
+        exit(1);
+    }
+    // remove the file socket
+    if let Err(err) = remove_file(tempfile_name) {
+        eprintln!("Failed to remove temporary file: {}", err);
+        exit(1);
+    }
+    println!("Filesystem {} successfully removed", socket_name);
 }
